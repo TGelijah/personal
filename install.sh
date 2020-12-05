db121615b9d9a9ee763d9f3352418988119861d3
Glorious <gloriouselijah125@gmail.com>
2020-12-05 07:30:57 +0000

    Uncomment install commands

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ cp vim/color/glo.vim ~/.vim/colors
 cp vim/.vimrc ~/
 
 
-#sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-#cp ohmyzsh/theme/glo.zsh-theme ~/.oh-my-zsh/themes
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+cp ohmyzsh/theme/glo.zsh-theme ~/.oh-my-zsh/themes
 cp ohmyzsh/.zshrc ~
 cp terminal/.aliases terminal/.hashes ~
 
