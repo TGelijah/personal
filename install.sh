3ec338834869980a35ec9e447abbf3dd1cfc356c
Glorious <gloriouselijah125@gmail.com>
2020-12-05 09:00:22 +0000

    Add nvm install to install.sh

--- install.sh
+++ install.sh
@@ -10,4 +10,11 @@ cp ohmyzsh/theme/glo.zsh-theme ~/.oh-my-zsh/themes
 cp ohmyzsh/.zshrc ~
 cp terminal/.aliases terminal/.hashes ~
 
-sudo pacman -Syu arduino calibre arduino-builder arduino-ctags atom audacious audacious-plugins audacity texinfo texlive-bibtexextra texlive-bin texlive-core texlive-fontsextra texlive-formatsextra texlive-games texlive-humanities texlive-latexextra texlive-music texlive-pictures texlive-pstricks texlive-publishers texlive-science texmaker texstudio telegram-desktop firefox-developer-edition geany gimp inkscape jre-openjdk jre-openjdk-headless lyx vim vim-runtime vlc blender pencil2d chromium code
+sudo pacman -Syu arduino calibre arduino-builder arduino-ctags atom audacious audacious-plugins audacity texinfo texlive-bibtexextra texlive-bin texlive-core texlive-fontsextra texlive-formatsextra texlive-games texlive-humanities texlive-latexextra texlive-music texlive-pictures texlive-pstricks texlive-publishers texlive-science texmaker texstudio telegram-desktop firefox-developer-edition geany gimp inkscape jre-openjdk jre-openjdk-headless lyx vim vim-runtime vlc blender pencil2d chromium code 
+
+#nvm for bash use
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+
+#nvm for zsh use
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | zsh
+
