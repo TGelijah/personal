fb1b4d5fd920c0e1ca6287f91498f53df39a10e4
Glorious <gloriouselijah125@gmail.com>
2020-12-25 12:18:16 +0000

    add bluetooth configuration

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+sudo pacman -Syu arduino openvpn pulseaudio-bluetooth alsa-plugins pulseaudio pulseaudio-alsa pavucontrol ponymix bluez bluez-utils calibre arduino-builder arduino-ctags atom audacious audacious-plugins audacity texinfo texlive-bibtexextra texlive-bin texlive-core texlive-fontsextra texlive-formatsextra texlive-games texlive-humanities texlive-latexextra texlive-music texlive-pictures texlive-pstricks texlive-publishers texlive-science texmaker texstudio telegram-desktop firefox-developer-edition geany gimp inkscape jre-openjdk jre-openjdk-headless lyx vim vim-runtime vlc blender pencil2d chromium code 
+
 mkdir ~/.vim
 mkdir ~/.vim/colors
 cp -r vim/color/tools ~/.vim/colors
@@ -10,13 +12,15 @@ cp ohmyzsh/theme/glo.zsh-theme ~/.oh-my-zsh/themes
 cp ohmyzsh/.zshrc ~
 cp terminal/.aliases terminal/.hashes ~
 
-sudo pacman -Syu arduino calibre arduino-builder arduino-ctags atom audacious audacious-plugins audacity texinfo texlive-bibtexextra texlive-bin texlive-core texlive-fontsextra texlive-formatsextra texlive-games texlive-humanities texlive-latexextra texlive-music texlive-pictures texlive-pstricks texlive-publishers texlive-science texmaker texstudio telegram-desktop firefox-developer-edition geany gimp inkscape jre-openjdk jre-openjdk-headless lyx vim vim-runtime vlc blender pencil2d chromium code 
 
 #nvm for bash use
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 
 #nvm for zsh use
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | zsh
+
+#bluetooth pulseaudio configuration
+sudo cp pulse/Client.conf /etc/pulse/
 
 #git configuration
 cp git/.gitmessage.txt ~
